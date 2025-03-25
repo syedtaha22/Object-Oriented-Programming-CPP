c532f5262125b1a0e89b74083ac08541505dd886
Syed Taha <syetaha@gmail.com>
2025-03-25 18:36:51 +0500

    refactor main.cpp to remove MemoryTracker and streamline game loop

--- hw-solutions/DungeonEscape/main.cpp
+++ hw-solutions/DungeonEscape/main.cpp
@@ -2,26 +2,19 @@
 
 #include "Game/core.hpp"
 #include "Utils/Printer.hpp"
-#include "MemoryTracker.hpp"
 
 int main() {
-    {
-        Game::Interface game("Player", "A brave adventurer", "dungeon.json");
-        Game::Interface::Menu();
-        game.hold();
+    Game::Interface game("Player", "A brave adventurer", "test.json");
+    Game::Interface::Menu();
+    game.hold();
 
-        Utils::Printer::ClearScreen();
+    Utils::Printer::ClearScreen();
 
-        while (!game.gameIsOver()) {
-            game.displayCurrentRoom();
-            game.fightEnemies();
-
-            while (!game.playerHasMoved()) game.promptUser();
-        }
+    while (!game.gameIsOver()) {
+        game.displayCurrentRoom();
+        game.fightEnemies();
+        while (!game.playerHasMoved()) game.promptUser();
     }
-
-    std::cout << "Memory Report:\n";
-    MemoryTracker::report();
 
     return 0;
 }
